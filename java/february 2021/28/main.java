2595a381be0e1203dcf00e510512b82cffaf7e82
= <=>
2021-02-28 16:23:10 -0700

    2-28-2020

---
+++ java/february 2021/28/main.java
@@ -0,0 +1,12 @@
+class Main {  
+  public static void main(String args[]) { 
+    System.out.println("Hello, world!");
+  }
+
+  public static void printOnePlusOne() {
+    //make an integer variable x with the initial value 1
+    //add one to x
+    //print out x
+    //call this function from main
+  }
+}
