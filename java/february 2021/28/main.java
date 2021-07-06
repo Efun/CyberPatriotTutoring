87b77df2577af2726b99a99f06cd6353e59938a5
= <=>
2021-07-05 22:03:14 -0600

    java changes

--- java/february 2021/28/main.java
+++ java/february 2021/28/main.java
@@ -1,6 +1,16 @@
 class Main {  
   public static void main(String args[]) { 
     System.out.println("Hello, world!");
+    printOnePlusOne();
+    ifStatementTest();
+
+    //use the alwaysReturnsFalse function in an if statement that prints "Pomegranate".
+  if (alwaysReturnsFalse()) {
+
+    System.out.println("Pomegranate");
+
+  }
+
   }
 
   public static void printOnePlusOne() {
@@ -9,5 +19,71 @@ class Main {
     //print out x
     //call this function from main
     //https://repl.it/languages/java10
+
+    //python
+    // x = 1
+    // x += 1
+    // print(str(x))
+
+    //java variable format: <type> <name> = <value>;
+    int x = 1;
+    x++;
+    System.out.println(x);
   }
+
+  //write a function that is going to use if statements called ifStatementTest
+
+  public static void ifStatementTest() {
+    boolean x = true;
+    boolean y = false;
+    //challenge: don't change any existing code, add one line to make it so that we print out Tangerine instead of potato
+
+    //python - if <condition>:
+    //java - if (<condition>) {} - parentheses are important!
+    if (x) {
+        System.out.println("X is true!");
+    }
+
+    y = true;
+    x = "potato";
+
+    //if y is not true then print "Potato"
+    //write an else block that prints "Tangerine"
+    if (!y) {
+      System.out.println("Potato.");
+    }
+    else {
+      System.out.println("Tangerine");
+    }
+  }
+
+  //write a function that returns false: alwaysReturnsFalse
+  public static boolean alwaysReturnsFalse() {
+    return(false); 
+  }
+
+
+  //public - you can access this item outside of the class that it's in
+  //private - you can only access this item inside of the class that it's in
+  //static - you do not have to instantiate a class to call a function
+  //void - there's no return
+  //return(type) - pass some value back
+  //; - every line in java ends with one of these
+  //{} - shows scope
+  //++ - increment operator adds 1 to whatever you use it on;
+  //-- - decrement operator subtracts 1 to whatever you use it on;
+  //! - not operator for booleans
+  //once we declare a variable, if we want to reassign it later, we do not have to put the type.
+  //function syntax: <access control> <?static> <return TYPE> <name>() { }
 }
+
+class Test {
+    public void printOnePlusOne() {
+        //do some code here
+    }
+
+    public static void printTwoPlusTwo() {
+        //do some other code here
+    }
+}
+
---
+++ java/february 2021/28/march 2021/main.java
@@ -0,0 +1,30 @@
+class Main {
+  public static void main(String[] args) {
+    //System.out.println("Hello world!");
+    boolean test1 = false;
+    boolean test2 = true;
+    test1 = true;
+    test2 = false;
+    
+    //write an if statement that will print out Hello World using the test1 variable!
+    if (!test1) {
+
+        System.out.println("Hello World"); 
+    }
+    else if (test2) {
+
+        System.out.println("banana");
+    }
+    //write an else statement that prints out potato
+    else {
+
+        System.out.println("potato");
+    }
+
+    //else if - write an else if statement that prints out banana if test2 is true
+    //in python - elif
+
+    //how can we make this if block print out banana by changing test1, test2 or both?
+    
+  }
+}
